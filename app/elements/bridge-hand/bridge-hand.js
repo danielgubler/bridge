834f73b7cd50e7f43018867d728e360845dc6c50
Daniel Gubler <danielcgubler@gmail.com>
2016-10-25 11:56:30 -0700

    Fix css for bridge-hand's width/height to contain cards

--- app/elements/bridge-hand/bridge-hand.js
+++ app/elements/bridge-hand/bridge-hand.js
@@ -1,4 +1,18 @@
 {
+  let suitOrder = {
+    diamonds: 1,
+    clubs: 2,
+    hearts: 3,
+    spades: 4
+  }
+
+  let numberOrder = {
+    jack: 11,
+    queen: 12,
+    king: 13,
+    ace: 14
+  }
+
   let id = "bridge-hand"
   class BridgeHand extends HTMLElement {
     createdCallback(){
@@ -9,6 +23,7 @@
     get cards() { return this._cards || [] }
     set cards(value) {
       this._cards = value || []
+      this._sort()
       this._update()
     }
 
@@ -28,8 +43,20 @@
         card.suit = data.suit
         card.number = data.number
         card.faceUp = this.faceUp
-        card.style.left = (25*idx) + "px"
-        card.style.zIndex = idx + 1
+        card.style.marginLeft = idx == 0 ? "0px" : "-75px"
+      })
+    }
+
+    _sort() {
+      this.cards.sort((a, b) => {
+        // sort by suit
+        if (suitOrder[a.suit] > suitOrder[b.suit]) { return -1 }
+        if (suitOrder[a.suit] < suitOrder[b.suit]) { return 1 }
+
+        // else sort by number
+        let aNumber = isNaN(a.number) ? numberOrder[a.number] : Number(a.number)
+        let bNumber = isNaN(b.number) ? numberOrder[b.number] : Number(b.number)
+        return aNumber > bNumber ? -1 : (aNumber == bNumber ? 0 : 1)
       })
     }
   }
